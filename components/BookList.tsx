27b9ddfa3e6d1727f0481fd19f8d3043575fe0ad
samnj <7932916+samnj@users.noreply.github.com>
2023-06-07 02:06:26 -0300

    modified BookList to pass user books to Book component

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -2,12 +2,15 @@
 
 import Book from "@/components/Book"
 import { TBook } from "@/lib/types"
+import useUserBooks from "@/hooks/useUserBooks"
 
 function BookList({ books }: { books: TBook[] }) {
+  const { userBooks } = useUserBooks()
+
   return (
     <div className="mt-6 grid grid-cols-4 gap-6">
       {books.map((book) => (
-        <Book key={book.id} book={book} />
+        <Book key={book.id} book={book} userBooks={userBooks} />
       ))}
     </div>
   )
