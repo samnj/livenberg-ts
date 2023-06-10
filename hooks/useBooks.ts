273e28cbf953154f18a1a475258ae058e7d62299
samnj <7932916+samnj@users.noreply.github.com>
2023-06-10 01:53:37 -0300

    fixed useBooks triggering fetch when the key was already cached

--- hooks/useBooks.ts
+++ hooks/useBooks.ts
@@ -7,6 +7,9 @@ import { TBooksResult } from "@/lib/types"
 import { useRouter } from "next/navigation"
 import { toast } from "react-hot-toast"
 import useSWRInfinite from "swr/infinite"
+import { useSWRConfig } from "swr"
+import { GUTENDEX_URL } from "@/lib/constants"
+import { useEffect } from "react"
 
 type useBooksProps = {
   query: string
@@ -14,13 +17,16 @@ type useBooksProps = {
 }
 
 function useBooks({ query, shouldFetch = true }: useBooksProps) {
+  const { cache } = useSWRConfig()
   const router = useRouter()
   const normalizedQuery = normalizeQuery(query)
 
   const getKey = createBookSearchKey(normalizedQuery, shouldFetch)
   const fetcher = createBookFetcher(query)
+
   const options = {
     shouldRetryOnError: false,
+    revalidateIfStale: false,
     onSuccess: (data: TBooksResult[]) => {
       if (!data) {
         toast("No results found")
@@ -31,6 +37,14 @@ function useBooks({ query, shouldFetch = true }: useBooksProps) {
     },
     onError: () => toast.error("Something went wrong"),
   }
+
+  useEffect(() => {
+    const isCached = !!cache.get(`${GUTENDEX_URL}?search=${normalizedQuery}`)
+
+    if (isCached) {
+      router.push(`/books?search=${normalizedQuery}`)
+    }
+  }, [cache, normalizedQuery, router])
 
   const { data, error, isLoading, size, setSize, isValidating } =
     useSWRInfinite<TBooksResult>(getKey, fetcher, options)
